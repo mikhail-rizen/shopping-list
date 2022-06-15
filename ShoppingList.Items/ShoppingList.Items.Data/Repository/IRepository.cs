0e22ba4f2f64697186110cc887a7b92ef0e678e4
Mikhail Rizen <michael.rizen@hotmail.com>
2022-06-15 13:07:44 +0300

    add repository pattern

---
+++ ShoppingList.Items/ShoppingList.Items.Data/Repository/IRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace ShoppingList.Items.Data.Repository
+{
+    public interface IRepository<TEntity> where TEntity: class, new()
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity> AddAsync(TEntity entity);
+        Task<TEntity> UpdateAsync(TEntity entity);
+    }
+}
