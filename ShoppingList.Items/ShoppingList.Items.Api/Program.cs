713bad133e74b2442b3d1f497e61a411a5c10a39
Mikhail Rizen <michael.rizen@hotmail.com>
2022-09-18 22:54:55 +0300

    enable swagger in production

--- ShoppingList.Items/ShoppingList.Items.Api/Program.cs
+++ ShoppingList.Items/ShoppingList.Items.Api/Program.cs
@@ -50,7 +50,7 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (true || app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
